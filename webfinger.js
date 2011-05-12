a8159baa69a0eb90f916e7e01f94a1d822e42d94
Bastian Hofmann <bhofmann@vz.net>
2011-05-12 10:43:05 -0700

    added some error handling

--- webfinger.js
+++ webfinger.js
@@ -12,9 +12,6 @@ function onOwnerReceived(response) {
         var owner = ownerResponse.getData();
 
         var emails = owner.getField(opensocial.Person.Field.EMAILS);
-        var email = emails[0].getField(opensocial.Email.Field.ADDRESS);
-        //loadHostMeta(email);
-        //return;
         for(var i = 0; i < emails.length; i++) {
             loadHostMeta(emails[i].getField(opensocial.Email.Field.ADDRESS));
         }
@@ -27,36 +24,48 @@ function loadHostMeta(email) {
     var params = {}; 
     params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.TEXT; 
     gadgets.io.makeRequest(url, function(response) { 
-        gadgets.log(response.text);
+        if (response.errors.length > 0) {
+            renderNotFound(email);
+            return;
+        }
+        gadgets.log(gadgets.json.stringify(response));
         var lrdd = response.text.match(/<Link\s*rel='lrdd'\s*template='(.*?)'/i);
         if (lrdd) {
-            loadLrdd(lrdd[1].replace('{uri}', email));
+            loadLrdd(lrdd[1].replace('{uri}', email), email);
         }
     }, params);
 }
 
-function loadLrdd(url) {
+function loadLrdd(url, email) {
     var params = {}; 
     params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.TEXT; 
     gadgets.io.makeRequest(url, function(response) { 
-        gadgets.log(response.text);
+        if (response.errors.length > 0) {
+            renderNotFound(email);
+            return;
+        }
         var portableContactsUrl = response.text.match(/<Link\s*rel='http:\/\/portablecontacts.net\/spec\/1.0#me'\s*href='(.*?)'/i);
         if (portableContactsUrl) {
-            loadPortableContacts(portableContactsUrl[1]);
+            loadPortableContacts(portableContactsUrl[1], email);
         }
     }, params);
 }
 
-function loadPortableContacts(url) {
+function loadPortableContacts(url, email) {
     url += '?fields=@all';
     var params = {}; 
     params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON; 
     gadgets.io.makeRequest(url, function(response) { 
-        renderProfile(response.data.entry);
+        if (response.errors.length > 0) {
+            renderNotFound(email);
+            return;
+        }
+        renderProfile(response.data.entry, email);
     }, params);
 }
 
 var template = '\
+<h3>{{original_email}}</h3>\
 <img src="{{thumbnailUrl}}" /> \
 <div id="information"><b>{{displayName}}</b>\
 <br />\
@@ -69,14 +78,22 @@ var template = '\
 </div><hr />\
 ';
 
-function renderProfile(data) {
+function renderProfile(data, email) {
     
     gadgets.log(gadgets.json.stringify(data));
     if (data) {
         $('#loading').hide();
-        $('#profile').append(Mustache.to_html(template, data));
+        data.original_email = email;
+        $('#profile').prepend(Mustache.to_html(template, data));
         gadgets.window.adjustHeight();
     }
+}
+
+var notFoundTemplate = '<b>{{original_email}}</b><br />No information found <hr />';
+function renderNotFound(email) {
+    $('#loading').hide();
+    $('#profile').prepend(Mustache.to_html(notFoundTemplate, {original_email: email}));
+    gadgets.window.adjustHeight();
 }
 
 if (typeof gadgets !== 'undefined' && gadgets.util && gadgets.util.registerOnLoadHandler) {
